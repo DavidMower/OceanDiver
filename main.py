aacafbf3a9077575c4cb43c0556423eee754bd3e
DavidMower84 <35422381+DavidMower84@users.noreply.github.com>
2019-06-22 22:02:33 +0100

    Update main.py
    Put the maingame loop into a function

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+# Scuba Diver
+# by David Mower davidmower84@gmail.com
+# Released under GNUv3 license
+
 import pygame, sys, time
 from pygame.locals import *
 pygame.init()
@@ -7,69 +11,75 @@ FPS = 60
 fpsClock = pygame.time.Clock()
 
 # create surface object
-DISPLAYSURF = pygame.display.set_mode((1024, 768))
+windowWidth = 1024
+windowHeight = 768
+DISPLAYSURF = pygame.display.set_mode((windowWidth, windowHeight))
 pygame.display.set_caption('Scuba Diver')
 
 # create the colours
+#         R    G    B
 WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 128)
-
-# create the scuba diver character
-diverImg = pygame.image.load('Images/ScubaDiver.png')
-diverx = 310
-divery = 310
-direction = 'right'
-
-# start playing background music
-pygame.mixer.music.load('Sounds/backgroundmusic.flac')
-pygame.mixer.music.play(-1, 0.0)
+GREEN = (  0, 255,   0)
+BLUE  = (  0,   0, 128)
 
 # main loop
-while True:
-    DISPLAYSURF.fill(WHITE)
+def main():
+    # create the scuba diver character
+    diverImg = pygame.image.load('Images/ScubaDiver.png')
+    diverx = 310
+    divery = 310
+    direction = 'right'
 
-    # title text
-    fontObj = pygame.font.Font('freesansbold.ttf', 32)
-    textSurfaceObj = fontObj.render('Scuba Diver' , True , GREEN, BLUE)
-    textRectObj = textSurfaceObj.get_rect()
-    textRectObj.center = (512, 100)
-    DISPLAYSURF.blit(textSurfaceObj, textRectObj)
+    # start playing background music
+    pygame.mixer.music.load('Sounds/backgroundmusic.flac')
+    pygame.mixer.music.play(-1, 0.0)
 
-    # test sound effect
-    #soundObj = pygame.mixer.Sound('Sounds/beeps.wav')
-    #soundObj.play()
-    #time.sleep(1) # let the sound play for 1 second
-    #soundObj.stop()
+    while True:
+        DISPLAYSURF.fill(WHITE)
 
-    # loops to keep diver on the move
-    if direction == 'right':
-        diverx += 5
-        if diverx == 725:
-            direction = 'down'
-    elif direction == 'down':
-        divery += 5
-        if divery == 470:
-            direction = 'left'
-    elif direction == 'left':
-        diverx -= 5
-        if diverx == 300:
-            direction = 'up'
-    elif direction == 'up':
-        divery -= 5
-        if divery == 300:
-            direction = 'right'
+        # title text
+        fontObj = pygame.font.Font('freesansbold.ttf', 32)
+        textSurfaceObj = fontObj.render('Scuba Diver' , True , GREEN, BLUE)
+        textRectObj = textSurfaceObj.get_rect()
+        textRectObj.center = (512, 100)
+        DISPLAYSURF.blit(textSurfaceObj, textRectObj)
 
-    # copy diverImg to DISPLAYSURF with coordinates
-    DISPLAYSURF.blit(diverImg, (diverx, divery))
+        # test sound effect
+        #soundObj = pygame.mixer.Sound('Sounds/beeps.wav')
+        #soundObj.play()
+        #time.sleep(1) # let the sound play for 1 second
+        #soundObj.stop()
 
-    for event in pygame.event.get():
-        # if event is quit, terminate the program
-        if event.type == QUIT:
-            pygame.mixer.music.stop()
-            pygame.quit()
-            sys.exit()
+        # loops to keep diver on the move
+        if direction == 'right':
+            diverx += 5
+            if diverx == 725:
+                direction = 'down'
+        elif direction == 'down':
+            divery += 5
+            if divery == 470:
+                direction = 'left'
+        elif direction == 'left':
+            diverx -= 5
+            if diverx == 300:
+                direction = 'up'
+        elif direction == 'up':
+            divery -= 5
+            if divery == 300:
+                direction = 'right'
 
-    # draw the surface object
-    pygame.display.update()
-    fpsClock.tick(FPS)
+        # copy diverImg to DISPLAYSURF with coordinates
+        DISPLAYSURF.blit(diverImg, (diverx, divery))
+
+        for event in pygame.event.get():
+            # if event is quit, terminate the program
+            if event.type == QUIT:
+                pygame.mixer.music.stop()
+                pygame.quit()
+                sys.exit()
+
+        # draw the surface object
+        pygame.display.update()
+        fpsClock.tick(FPS)
+
+main()
