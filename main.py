45a7114ac6ca3a6d4cf8aa8cc6a4600c754d22cc
DavidMower84 <35422381+DavidMower84@users.noreply.github.com>
2019-06-22 21:39:27 +0100

    Update main.py
    Added some test Title text and move sprint animation to middle of the surface object

--- main.py
+++ main.py
@@ -3,44 +3,55 @@ from pygame.locals import *
 pygame.init()
 
 # Frames per second setting
-FPS = 30
+FPS = 60
 fpsClock = pygame.time.Clock()
 
 # Create surface object
 DISPLAYSURF = pygame.display.set_mode((1024, 768))
 pygame.display.set_caption('Ocean Diver')
 
+# Create the colours
 WHITE = (255, 255, 255)
-catImg = pygame.image.load('Images/ScubaDiver.png')
-catx = 10
-caty = 10
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 128)
+
+# Create the scuba diver character
+diverImg = pygame.image.load('Images/ScubaDiver.png')
+diverx = 310
+divery = 310
 direction = 'right'
 
 # Main loop
 while True:
     DISPLAYSURF.fill(WHITE)
 
+    # Title Text
+    fontObj = pygame.font.Font('freesansbold.ttf', 32)
+    textSurfaceObj = fontObj.render('Scuba Diver' , True, GREEN, BLUE)
+    textRectObj = textSurfaceObj.get_rect()
+    textRectObj.center = (512, 100)
+    DISPLAYSURF.blit(textSurfaceObj, textRectObj)
+
+    # Loops to keep diver on the move
     if direction == 'right':
-        catx += 5
-        if catx == 280:
+        diverx += 5
+        if diverx == 725:
             direction = 'down'
-    
     elif direction == 'down':
-        caty += 5
-        if caty == 220:
+        divery += 5
+        if divery == 470:
             direction = 'left'
-
     elif direction == 'left':
-        catx -= 5
-        if catx == 10:
+        diverx -= 5
+        if diverx == 300:
             direction = 'up'
-
     elif direction == 'up':
-        caty -=5
-        if caty == 10:
+        divery -= 5
+        if divery == 300:
             direction = 'right'
 
-    DISPLAYSURF.blit(catImg, (catx, caty))
+    # Copy diverImg to DISPLAYSURF with coordinates
+    DISPLAYSURF.blit(diverImg, (diverx, divery))
 
     for event in pygame.event.get():
         # If event is quit, terminate the program
