b028db163370b5e1134fb993f2d5d1e7e55f6678
DavidMower84 <35422381+DavidMower84@users.noreply.github.com>
2019-06-23 06:15:23 +0100

    More music and main.py refactored
    Created seperate music files. Changed main meu title text to reuse writeText function

--- main.py
+++ main.py
@@ -4,18 +4,42 @@
 import pygame, sys, time
 from pygame.locals import *
 
-# Initialisations
+# Init
 FPS = 60 # frames per second setting
 assert(FPS <= 0, 'FPS must be a positive integer, for example 30 or 60')
-defaultFontSize = 32
+defaultFontSize = 32 # default font size for text
 
-# create colours  R    G    B
-colourBlack   = (  0,   0,   0)
-colourBlue    = (  0,   0, 128)
-colourGreen   = (  0, 255,   0)
-colourRed     = (155,   0,   0)
-colourWhite   = (255, 255, 255)
-colourYellow  = (155, 155,   0)
+# create the colours  R    G    B
+colourBlack       = (  0,   0,   0)
+colourBlue        = (  0,   0, 128)
+colourGreen       = (  0, 255,   0)
+colourRed         = (155,   0,   0)
+colourWhite       = (255, 255, 255)
+colourYellow      = (155, 155,   0)
+
+# terminates the game when called
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+# creates the Surface and Rect objects for some text
+def writeText(aText, aColour, aBackgroundColour, aTop, aLeft):
+    textSurf = defaultFont.render(aText, True, aColour, aBackgroundColour)
+    textRect = textSurf.get_rect()
+    textRect.topleft = (aTop, aLeft)
+    return (textSurf, textRect)
+
+# waits for any key to be pressed
+def checkForKeyPress():
+    if len(pygame.event.get(QUIT)) > 0:
+        terminate()
+
+    keyUpEvents = pygame.event.get(KEYUP)
+    if len(keyUpEvents) == 0:
+        return None
+    if keyUpEvents[0].key == K_ESCAPE:
+        terminate()
+    return keyUpEvents[0].key
 
 # main loop
 def main():
@@ -33,6 +57,46 @@ def main():
     while True:
         runGameLoop()
 
+# main menu loop
+def showMainMenu():
+    global newGameSurf, newGameRect, loadGameSurf, loadGameRect, saveGameSurf, saveGameRect, howToPlaySurf, howToPlayRect, optionsSurf, optionsRect, highScoresSurf, highScoresRect, quitSurf, quitRect
+
+    while True:
+        displaySurf.fill(colourWhite)
+
+        # start playing main-menu music
+        pygame.mixer.music.load('Sounds/MainMenu.flac')
+        pygame.mixer.music.play(-1, 0.0)
+
+        # draw the main menu title text
+        titleSurf, titleRect = writeText('Scuba Diver', colourGreen, colourBlue, windowWidth - 612, windowHeight - 620)
+        displaySurf.blit(titleSurf, titleRect)
+
+        # draw the main menu buttons
+        newGameSurf, newGameRect       = writeText('Start New Game', colourBlack, colourWhite, windowWidth - 612, windowHeight - 520)
+        displaySurf.blit(newGameSurf, newGameRect)
+        loadGameSurf, loadGameRect     = writeText(  'Load Game',    colourBlack, colourWhite, windowWidth - 612, windowHeight - 470)
+        displaySurf.blit(loadGameSurf, loadGameRect)
+        saveGameSurf, saveGameRect     = writeText(  'Save Game',    colourBlack, colourWhite, windowWidth - 612, windowHeight - 420)
+        displaySurf.blit(saveGameSurf, saveGameRect)
+        howToPlaySurf, howToPlayRect   = writeText( 'How to Play',   colourBlack, colourWhite, windowWidth - 612, windowHeight - 370)
+        displaySurf.blit(howToPlaySurf, howToPlayRect)
+        optionsSurf, optionsRect       = writeText(   'Options',     colourBlack, colourWhite, windowWidth - 612, windowHeight - 320)
+        displaySurf.blit(optionsSurf, optionsRect)
+        highScoresSurf, highScoresRect = writeText( 'High Scores',   colourBlack, colourWhite, windowWidth - 612, windowHeight - 270)
+        displaySurf.blit(highScoresSurf, highScoresRect)
+        quitSurf, quitRect             = writeText(    'Quit',       colourBlack, colourWhite, windowWidth - 612, windowHeight - 220)
+        displaySurf.blit(quitSurf, quitRect)
+
+        # temp loop so I can test the menu
+        if checkForKeyPress():
+            pygame.event.get() # clear event queue
+            pygame.mixer.music.stop() # stop the main menu background music
+            return
+        pygame.display.update()
+        fpsClock.tick(FPS)
+
+# main in-game loop
 def runGameLoop():
     # create the scuba diver character
     diverImg = pygame.image.load('Images/ScubaDiver.png')
@@ -40,8 +104,8 @@ def runGameLoop():
     divery = 310
     direction = 'right'
 
-    # start playing background music
-    pygame.mixer.music.load('Sounds/backgroundmusic.flac')
+    # start playing CoastalDive background music
+    pygame.mixer.music.load('Sounds/CoastalDive.flac')
     pygame.mixer.music.play(-1, 0.0)
 
     while True:
@@ -84,63 +148,5 @@ def runGameLoop():
         pygame.display.update()
         fpsClock.tick(FPS)
 
-def showMainMenu():
-    global newGameSurf, newGameRect, loadGameSurf, loadGameRect, saveGameSurf, saveGameRect, howToPlaySurf, howToPlayRect, optionsSurf, optionsRect, highScoresSurf, highScoresRect, quitSurf, quitRect
-    
-    while True:
-        displaySurf.fill(colourWhite)
-
-        # main menu title text
-        titleFont = defaultFont.render('Scuba Diver' , True , colourGreen, colourBlue)
-        textRectObj = titleFont.get_rect()
-        textRectObj.center = (512, 150)
-        displaySurf.blit(titleFont, textRectObj)
-
-        # store the main menu buttons and their rectangles
-        newGameSurf, newGameRect       = makeText('Start New Game', colourBlack, colourWhite, windowWidth - 612, windowHeight - 520)
-        loadGameSurf, loadGameRect     = makeText('Load Game', colourBlack, colourWhite, windowWidth - 612, windowHeight - 470)
-        saveGameSurf, saveGameRect     = makeText('Save Game', colourBlack, colourWhite, windowWidth - 612, windowHeight - 420)
-        howToPlaySurf, howToPlayRect   = makeText('How to Play', colourBlack, colourWhite, windowWidth - 612, windowHeight - 370)
-        optionsSurf, optionsRect       = makeText('Options', colourBlack, colourWhite, windowWidth - 612, windowHeight - 320)
-        highScoresSurf, highScoresRect = makeText('High Scores', colourBlack, colourWhite, windowWidth - 612, windowHeight - 270)
-        quitSurf, quitRect             = makeText('Quit', colourBlack, colourWhite, windowWidth - 612, windowHeight - 220)
-
-        # draw the main menu buttons
-        displaySurf.blit(newGameSurf, newGameRect)
-        displaySurf.blit(loadGameSurf, loadGameRect)
-        displaySurf.blit(saveGameSurf, saveGameRect)
-        displaySurf.blit(howToPlaySurf, howToPlayRect)
-        displaySurf.blit(optionsSurf, optionsRect)
-        displaySurf.blit(highScoresSurf, highScoresRect)
-        displaySurf.blit(quitSurf, quitRect)
-
-        if checkForKeyPress():
-            pygame.event.get() # clear event queue
-            return
-        pygame.display.update()
-        fpsClock.tick(FPS)
-
-# terminates the game when called
-def terminate():
-    pygame.quit()
-    sys.exit()
-
-def makeText(text, color, bgcolor, top, left):
-    # create the Surface and Rect objects for some text.
-    textSurf = defaultFont.render(text, True, color, bgcolor)
-    textRect = textSurf.get_rect()
-    textRect.topleft = (top, left)
-    return (textSurf, textRect)
-
-def checkForKeyPress():
-    if len(pygame.event.get(QUIT)) > 0:
-        terminate()
-
-    keyUpEvents = pygame.event.get(KEYUP)
-    if len(keyUpEvents) == 0:
-        return None
-    if keyUpEvents[0].key == K_ESCAPE:
-        terminate()
-    return keyUpEvents[0].key
-
+# start the main() loop function
 main()
