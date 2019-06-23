77b2563139fe5812f4bd1a39b32bdcf73879001f
DavidMower84 <35422381+DavidMower84@users.noreply.github.com>
2019-06-23 22:04:13 +0100

    added example code for classes
    example getter and setters added

--- main.py
+++ main.py
@@ -6,7 +6,16 @@
 import random, sys, copy, os, pygame
 from pygame.locals import *
 from settings import *
+  
    
+# class useMyVars(object):
+#    def __init__(self, myVar):
+#        self.myVar = myVar
+#    def getMyVar(self):
+#        return self.myVar
+#    def setMyVar(self, myVar):
+#       self.myVar = myVar
+
 
 def main():
     # pygame initialisation
