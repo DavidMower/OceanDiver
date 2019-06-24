18d1ae88d1fbb1e59d517aab22ab6b4a4ef91d53
DavidMower84 <35422381+DavidMower84@users.noreply.github.com>
2019-06-24 17:30:10 +0100

    Created player.py
    Created player.py to move character functions into. Will create getters and setters for each character attribute

---
+++ player.py
@@ -0,0 +1,12 @@
+# character.py
+import pygame
+from pygame.locals import *
+from settings import *
+
+# class useMyVars(object):
+#    def __init__(self, myVar):
+#        self.myVar = myVar
+#    def getMyVar(self):
+#        return self.myVar
+#    def setMyVar(self, myVar):
+#       self.myVar = myVar
