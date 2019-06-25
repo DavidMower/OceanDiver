03217c012f0073093bce870e7fc36fa715443f7a
DavidMower84 <35422381+DavidMower84@users.noreply.github.com>
2019-06-25 06:57:34 +0100

    Update hud.py
    Improved commenting around te code and variable names used. Also created a new version of drawHealthBar ready to be tested

--- hud.py
+++ hud.py
@@ -6,22 +6,40 @@ import pygame
 from settings import *
 from main import *
 
-
 # creates a health bar
-def drawHealthBar(aPlayerHealth):
-    for c in range(aPlayerHealth): # draw the red health bars
-        pygame.draw.rect(displaySurf, colourRed, (15, 20 + (10 * player1.getHealth()) - c * 10, 20, 10))
-    for m in range(player1.getHealth()):
-        pygame.draw.rect(displaySurf, colourBlack, (15, 20 + (10 * player1.getHealth()) - m * 10, 20, 10), 1)
+# requires an integer value as an argument which the current amount of health
+def drawHealthBar(aPlayersHealth):
+    for eachHealth in range(aPlayersHealth):
+        # draw a red health rectangle for each oxygen value
+        pygame.draw.rect(displaySurf, colourRed, (15, 20 + (10 * player1.getHealth()) - eachHealth * 10, 20, 10))
+    for eachHealth2 in range(player1.getHealth()):
+        # draw a black background rectangle for each oxygen value
+        pygame.draw.rect(displaySurf, colourBlack, (15, 20 + (10 * player1.getHealth()) - eachHealth2 * 10, 20, 10), 1)
+    # write H above the health bar
     healthBarSurf, healthBarRect = writeText('H', colourGreen, colourBlue, 5, 5)
     healthBarSurf.blit(healthBarSurf, healthBarRect)
 
-
 # creates a oxygen bar
-def drawOxygenBar(aPlayerHealth):
-    for c in range(aPlayerHealth): # draw the blue oxygen bars 
-        pygame.draw.rect(displaySurf, colourBlue, (windowWidth - 35, 25 + (10 * player1.getOxygen()) - c * 10, 20, 10))
-    for m in range(player1.getOxygen()):
-        pygame.draw.rect(displaySurf, colourBlack, (windowWidth - 35, 25 + (10 * player1.getOxygen()) - m * 10, 20, 10), 1)
+# requires an integer value as an argument which represents the current amount of oxygen
+def drawOxygenBar(aPlayersOxygen):
+    for eachOxygen in range(aPlayersOxygen):
+        # draw a blue oxygen rectangle for each oxygen value
+        pygame.draw.rect(displaySurf, colourBlue, (windowWidth - 35, 25 + (10 * player1.getOxygen()) - eachOxygen * 10, 20, 10))
+    for eachOxygen2 in range(player1.getOxygen()):
+        # draw a black background rectangle for each oxygen value
+        pygame.draw.rect(displaySurf, colourBlack, (windowWidth - 35, 25 + (10 * player1.getOxygen()) - eachOxygen2 * 10, 20, 10), 1)
+    # write O above the oxygen bar
     oxygenBarSurf, oxygenBarRect = writeText('O', colourGreen, colourBlue, 20, 20)
     oxygenBarSurf.blit(oxygenBarSurf, oxygenBarRect)
+
+# creates a health bar
+# requires an integer value as an argument which the current amount of health
+#def drawHealthBar(aPlayersHealth):
+#    for eachHealth in range(aPlayersHealth):
+#        # draw a red health rectangle for each oxygen value
+#        pygame.draw.rect(displaySurf, colourRed, (15, 20 + (10 * aPlayersHealth - eachHealth * 10, 20, 10))
+#       # draw a black background rectangle for each oxygen value
+#        pygame.draw.rect(displaySurf, colourBlack, (15, 20 + (10 * aPlayersHealth - eachHealth * 10, 20, 10), 1)
+#    # write H above the health bar
+#    healthBarSurf, healthBarRect = writeText('H', colourGreen, colourBlue, 5, 5)
+#    healthBarSurf.blit(healthBarSurf, healthBarRect)
